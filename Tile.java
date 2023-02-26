cd867a86c98c5800ff9524bc5c453d4107f99dcd
Riley6215 <70374862+Riley6215@users.noreply.github.com>
2023-02-26 12:20:58 -0600

    future proofing the tile class for when we add other npc/bosses

--- Tile.java
+++ Tile.java
@@ -5,7 +5,16 @@ public class Tile { // the fundamental type of the map can be various types
     //Solid/cant go through/on: Wall
 
     private int type;
-
+/*
+* For use later
+* Private Boss (type 4) (RED)
+* private enemy (type 3) (Pink)
+* private NPC (type 2) (green)
+* Floors will be white
+* Walls will be black until we get images to load into these locations.
+* Player will be yellow?
+* These classes will have their own info, but we can use it to generate the other classes/ set the boss enemy as needed
+* */
 
     public Tile(int tileType){ // 0 is floor 1 is wall 2:NPC 3:enemy 4:Boss
         this.type = tileType;
