0be4657ee1787294303e21fdb3410f7526e3d527
Riley6215 <70374862+Riley6215@users.noreply.github.com>
2023-02-26 14:33:41 -0600

    Added World display based off of the creation of tile types

--- World.java
+++ World.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
 public class World {
     /*
     * Class Will load the world in ie the iterations of tiles and display it as a canvas
@@ -58,8 +62,82 @@ public class World {
 
     public Tile getTile(int x, int y){return this.worldMap[y][x];}
 
-    public void displayWorld(){
+    public void displayWorld(){ //tiles are 8x8 pixels generated from (0,0) to (8*length, 8*height) (x,y) respectively
+        final String title = "Game World: Software Chasing";
+        final int width = this.length * 16;
+        final int height = this.height * 16;
 
+        //Creating the frame.
+        JFrame frame = new JFrame(title);
+
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        //Creating the canvas.
+        Canvas canvas = new Canvas();
+
+        canvas.setSize(width, height);
+        //canvas.setBackground(Color.BLACK);
+        canvas.setVisible(true);
+        canvas.setFocusable(false);
+
+        //Putting it all together.
+        frame.add(canvas);
+        canvas.createBufferStrategy(3);
+
+
+        BufferStrategy bufferStrategy;
+        Graphics graphics;
+
+        bufferStrategy = canvas.getBufferStrategy();
+        graphics = bufferStrategy.getDrawGraphics();
+
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.length; x++) {
+                //get color of tile based on type
+                int type = this.worldMap[y][x].getType();
+                if (type == 0) {graphics.setColor(Color.CYAN);}else {graphics.setColor(Color.ORANGE);}
+
+                /*TODO*/
+                graphics.fillRect(x*16,y*16,16,16); //TILE IMAGE WILL GO HERE LATER
+            }
+        }
+
+        while (true) { // will add movements in here and wait for certain motions to keep displaying this
+            bufferStrategy = canvas.getBufferStrategy();
+            graphics = bufferStrategy.getDrawGraphics();
+//            graphics.clearRect(0, 0, width, height);
+
+//            for (int y = 0; y < this.height; y++) {
+//                for (int x = 0; x < this.length; x++) {
+//                    //get color of tile based on type
+//                    int type = this.worldMap[y][x].getType();
+//                    if (type == 0) {graphics.setColor(Color.CYAN);}else {graphics.setColor(Color.ORANGE);}
+//                    graphics.fillRect(x*16,y*16,16,16);
+//                }
+//            }
+
+
+            bufferStrategy.show();
+            graphics.dispose();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] tiles = new int[50][50];
+        int count = 1;
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 50; j++) {
+                tiles[i][j] = (count + i)% 2;
+                count++;
+            }
+        }
+        World test = new World(50,50,tiles);
+        test.displayWorld();
     }
 
 }// END CLASS
