4aac02f9af05d53ac8034984437ee3af35831d13
Saheer <saheerahmad1@gmail.com>
2023-02-27 16:58:09 -0500

    Added Character Creation

--- CharSelect.java
+++ CharSelect.java
@@ -10,9 +10,16 @@ public class CharSelect {
     JPanel menu;
     JPanel titlePanel;
     JPanel editPanel;
-    static String char1Name = "Character 1";
-    static String char2Name = "Character 2";
-    static String char3Name = "Character 3";
+
+    String username = "Saheer";//Get current users username
+
+    static Player char1 = null;//Get however many characters for the user and set the ones that exist(Hunter).
+    static Player char2 = null;
+    static Player char3 = null;
+    
+    static String char1Name = null;
+    static String char2Name = null;
+    static String char3Name = null;
 
     public CharSelect(String name1, String name2, String name3){
         window = new JFrame();
@@ -27,19 +34,42 @@ public class CharSelect {
         titlePanel.setBounds(100, 100, 600, 100);
         titlePanel.setBackground(Color.black);
         JLabel titleName = new JLabel("Software Chasing: \n A BoilerMaker Saga");
+        titleName.setForeground(Color.white);
         titleName.setFont(new Font("Times New Roman", Font.PLAIN, 35));;
         titlePanel.add(titleName);
         con.add(titlePanel);
 
+        JButton createCharButton = new JButton();
+        createCharButton.setBounds(200, 250, 400, 40);
+        createCharButton.setBackground(Color.white);
+        createCharButton.setText("Create new Character");
+        createCharButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createCharPanel();
+            }
+            
+        });
+        if(char1Name == null | char2Name == null | char3Name == null){
+            con.add(createCharButton);
+        }
+
         JButton char1Button = new JButton();
-        char1Button.setBounds(270, 250, 200, 60);
+        char1Button.setBounds(270, 300, 200, 60);
         char1Button.setBackground(Color.white);
         char1Button.setText(name1);
-        
-        con.add(char1Button);
+        char1Button.addActionListener(new ActionListener(){
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Open Game and send in Character 1
+            }
+            
+        });
+        
         JButton char1EditButton = new JButton();
-        char1EditButton.setBounds(480, 250, 60, 60);
+        char1EditButton.setBounds(480, 300, 60, 60);
         char1EditButton.setBackground(Color.white);
         char1EditButton.setText("Edit");
         char1EditButton.addActionListener(new ActionListener(){
@@ -50,16 +80,26 @@ public class CharSelect {
             }
             
         });
-        con.add(char1EditButton);
+        if(char1Name != null){
+            con.add(char1Button);
+            con.add(char1EditButton);
+        }
 
         JButton char2Button = new JButton();
-        char2Button.setBounds(270, 330, 200, 60);
+        char2Button.setBounds(270, 380, 200, 60);
         char2Button.setBackground(Color.white);
         char2Button.setText(name2);
-        con.add(char2Button);
+        char2Button.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Open Game and send in Character 2
+            }
+            
+        });
 
         JButton char2EditButton = new JButton();
-        char2EditButton.setBounds(480, 330, 60, 60);
+        char2EditButton.setBounds(480, 380, 60, 60);
         char2EditButton.setBackground(Color.white);
         char2EditButton.setText("Edit");
         char2EditButton.addActionListener(new ActionListener(){
@@ -70,16 +110,26 @@ public class CharSelect {
             }
             
         });
-        con.add(char2EditButton);
+        if(char2Name != null){
+            con.add(char2Button);
+            con.add(char2EditButton);
+        }
 
         JButton char3Button = new JButton();
-        char3Button.setBounds(270, 410, 200, 60);
+        char3Button.setBounds(270, 460, 200, 60);
         char3Button.setBackground(Color.white);
         char3Button.setText(name3);
-        con.add(char3Button);
+        char3Button.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Open Game and send in Character 3
+            }
+            
+        });
 
         JButton char3EditButton = new JButton();
-        char3EditButton.setBounds(480, 410, 60, 60);
+        char3EditButton.setBounds(480, 460, 60, 60);
         char3EditButton.setBackground(Color.white);
         char3EditButton.setText("Edit");
         char3EditButton.addActionListener(new ActionListener(){
@@ -90,7 +140,10 @@ public class CharSelect {
             }
             
         });
-        con.add(char3EditButton);
+        if(char3Name != null){
+            con.add(char3Button);
+            con.add(char3EditButton);
+        }
 
     }
 
@@ -134,7 +187,7 @@ public class CharSelect {
                 window.dispose();
                 editFrame.dispose();
 
-                if(character.equals("char1")){
+                if(character.equals("char1")){//Save new character name(Hunter)
                     char1Name = CharNameText.getText();
                     new CharSelect(char1Name, char2Name, char3Name);
                 }else if(character.equals("char2")){
@@ -163,6 +216,118 @@ public class CharSelect {
             
         });
         editCon.add(cancelButton);
+    }
+    public void createCharPanel(){
+        JFrame createFrame = new JFrame();
+        Container createCon = new Container();
+        createFrame.setSize(800,600);
+        createFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        createFrame.getContentPane().setBackground(Color.black);
+        createFrame.setLayout(null);
+        createFrame.setVisible(true);
+        createCon = createFrame.getContentPane();
+
+        JPanel createTitlePanel = new JPanel();
+        createTitlePanel.setBounds(100, 50, 600, 100);
+        createTitlePanel.setBackground(Color.black);
+        JLabel createTitleName = new JLabel("Create New Character");
+        createTitleName.setForeground(Color.white);
+        createTitleName.setFont(new Font("Times New Roman", Font.PLAIN, 35));
+        createTitlePanel.add(createTitleName);
+        createCon.add(createTitlePanel);
+
+        JPanel form = new JPanel();
+        form.setSize(250,80);
+        form.setLocation(250, 200);
+        form.setBackground(Color.blue);
+
+        JLabel charNameLabel = new JLabel("Character Name: ");
+        charNameLabel.setSize(200, 25);
+        charNameLabel.setFont(new Font("Acumin Pro", Font.PLAIN, 18));
+        charNameLabel.setLocation(150, 150);
+        charNameLabel.setForeground(Color.white);
+        createCon.add(charNameLabel);
+        JTextField charNameField = new JTextField();
+        charNameField.setFont(new Font("Acumin Pro", Font.PLAIN, 18));
+        charNameField.setSize(300, 30);
+        charNameField.setLocation(350, 150);
+        createCon.add(charNameField);
+
+        JLabel typeLabel = new JLabel();
+        typeLabel.setText("Please select the type of character!");
+        typeLabel.setLocation(150, 200);
+        typeLabel.setSize(200, 25);
+        typeLabel.setFont(new Font("Acumin Pro", Font.PLAIN, 18));
+        typeLabel.setForeground(Color.white);
+
+        ButtonGroup typeGroup = new ButtonGroup();
+        JRadioButton type1 = new JRadioButton();
+        type1.setText("Type 1");
+        type1.setLocation(150, 300);
+        JRadioButton type2 = new JRadioButton();
+        type2.setText("Type 2");
+        type2.setLocation(150, 300);
+        JRadioButton type3 = new JRadioButton();
+        type3.setText("Type 3");
+        type3.setLocation(150, 300);
+        typeGroup.add(type1);
+        typeGroup.add(type2);
+        typeGroup.add(type3);
+        
+        form.add(typeLabel);
+        form.add(type1);
+        form.add(type2);
+        form.add(type3);
+        createCon.add(form);
+
+        JButton createCharButton = new JButton();
+        createCharButton.setBounds(270, 400, 100, 50);
+        createCharButton.setBackground(Color.white);
+        createCharButton.setText("Create");
+        createCharButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window.dispose();  
+                Player newPlayer;              
+                if(type1.isSelected()){ //Save newPlayer to database(Hunter)
+                    newPlayer = new Player(username, charNameField.getText(), 1);
+                }else if(type2.isSelected()){
+                    newPlayer = new Player(username, charNameField.getText(), 2);
+                } else{
+                    newPlayer = new Player(username, charNameField.getText(), 3);
+                }
+                if(char1Name == null){//temporary
+                    char1Name = charNameField.getText();
+                    char1 = newPlayer;
+                } else if(char2Name == null){
+                    char2Name = charNameField.getText();
+                    char2 = newPlayer;
+                } else if(char3Name == null){
+                    char3Name = charNameField.getText();
+                    char3 = newPlayer;
+                }
+                createFrame.dispose();
+                new CharSelect(char1Name, char2Name, char3Name);
+
+            }
+            
+        });
+        createCon.add(createCharButton);
+
+        JButton cancelButton = new JButton();
+        cancelButton.setBounds(390, 400, 100, 50);
+        cancelButton.setBackground(Color.white);
+        cancelButton.setText("Cancel");
+        cancelButton.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                createFrame.dispose();
+            }
+            
+        });
+        createCon.add(cancelButton);
     }
 
     public static void main(String args[]) {
